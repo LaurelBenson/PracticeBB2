47f26ab31976a80667fbe458dbb74418c601a917
LaurelBenson <laurel@twentyideas.com>
2019-11-04 22:02:47 -0800

    Adding quasar

---
+++ practicebb2/src/quasar.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+
+import './styles/quasar.styl'
+import iconSet from 'quasar/icon-set/eva-icons.js'
+import '@quasar/extras/eva-icons/eva-icons.css'
+import {
+  Quasar, 
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+} from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+  },
+  directives: {
+  },
+  plugins: {
+  },
+  iconSet: iconSet
+ })
